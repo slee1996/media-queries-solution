da1ddae02db587538ac956344d6aadaa882755a5
Spencer Lee <spencerlee96@gmail.com>
2019-11-20 16:49:13 -0700

    it is a website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+library.add(faBars)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      toggleMenu: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({
+      toggleMenu: !this.state.toggleMenu
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className='nav-div'>
+            <section id='bootstrap-bill'>Start Bootstrap</section>
+            <nav id='nav-bar'>
+              <a id='link'>Services</a>
+              <a id='link'>Portfolio</a>
+              <a id='link'>About</a>
+              <a id='link'>Team</a>
+              <a id='link'>Contact</a>
+            </nav>
+            <button id='hamberder' onClick={this.handleToggle}>Menu <FontAwesomeIcon icon='bars'  /></button>
+          </div>
+          {!this.state.toggleMenu ?
+            null
+            :
+            <nav id='menu'>
+              <a>Services</a>
+              <a>Portfolio</a>
+              <a>About</a>
+              <a>Team</a>
+              <a>Contact</a>
+            </nav>}
+          <div className='welcome-div'>
+            <section id='studio'>Welcome To Our Studio!</section>
+            <section id='meet-you'>it's nice to meet you</section>
+            <button id='tell-me'>Tell Me More</button>
+          </div>
+        </header>
+        
+        <body className='app-body'>
+          <section id='services'>Services</section>
+          <section id='portfolio'>Portfolio</section>
+          <section id='about'>About</section>
+          <section id='team'>Team</section>
+          <section id='contact'>Contact</section>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
